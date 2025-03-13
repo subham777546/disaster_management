160d3e775aef56814238d19501f13a5cdaae2e4e
subham777546 <subhampatnaik0111@gmail.com>
2025-03-13 19:20:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReportDisaster from './ReportDisaster';
+import DisasterList from './DisasterList';
+
+function App() {
+  return (
+    <div>
+      <h1>Disaster Management System</h1>
+      <ReportDisaster />
+      <DisasterList />
+    </div>
+  );
+}
+
+export default App;
